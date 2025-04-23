672accaeeb3bb8a810ae10bd8f4066e3304765cf
Yoni190 <yfisuhu@gmail.com>
2025-04-23 08:48:44 +0300

    Implement next button functionality

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import {greeting} from "./greeting.js";
 import "./style.css";
 
-console.log(greeting);
+
+const images = document.querySelectorAll('.image');
+let previousIndex = 0;
+
+const nextButton = document.querySelector('#next');
+nextButton.addEventListener('click', ()=>{
+    images[previousIndex].className = 'image';
+    if(previousIndex + 1 >= images.length){
+        previousIndex = -1;
+    }
+    images[previousIndex + 1].className = 'image active';
+    previousIndex++;
+})
