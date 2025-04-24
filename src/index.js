9686c64cc12a37acf423f1f53119c938f9bdcc90
Yoni190 <yfisuhu@gmail.com>
2025-04-24 14:18:48 +0300

    Create functions to change status of the dots

--- src/index.js
+++ src/index.js
@@ -11,24 +11,49 @@ let currentIndex = 0;
 
 for(let i = 0; i < imageCount; i++){
     const dot = document.createElement('p');
-    dot.innerHTML = 'O';
-    dot.className = `circle ${i}`;
+    if(i === 0){
+        dot.innerHTML = '●';
+    }
+    else {
+        dot.innerHTML = '○';
+    }
+    dot.className = `circle-${i}`;
     circlesContainer.appendChild(dot);
 }
 
-const dots = document.querySelectorAll('.circle');
+const dots = document.querySelectorAll('[class^="circle-"]');
+
+const changePreviousDot = (index) => {
+    const previousDot = document.querySelector(`.circle-${index}`);
+    previousDot.innerHTML = '○';
+}
+
+const changeNewDot = (index) => {
+    const dot = document.querySelector(`.circle-${index}`)
+    dot.innerHTML = '●';
+}
 
 
 const moveForward = () => {
     //Current image becomes hidden
     images[currentIndex].className = 'image';
+    // const previousDot = document.querySelector(`.circle-${currentIndex}`);
+    // previousDot.innerHTML = '○';
+
+    changePreviousDot(currentIndex);
     //Check if index reaches the end
     if(currentIndex + 1 >= images.length){
         currentIndex = -1;
     }
     //Shows next image
     images[currentIndex + 1].className = "image active";
+    
+    console.log(currentIndex)
+
     currentIndex++;
+    // const dot = document.querySelector(`.circle-${currentIndex}`)
+    // dot.innerHTML = '●';
+    changeNewDot(currentIndex);
 }
 
 const nextButton = document.querySelector('#next');
@@ -37,20 +62,24 @@ nextButton.addEventListener('click', moveForward);
 const previousButton = document.querySelector('#previous');
 previousButton.addEventListener('click', ()=>{
     images[currentIndex].className = 'image';
+    changePreviousDot(currentIndex);
     if(currentIndex - 1 < 0){
         currentIndex = images.length;
     }
     images[currentIndex - 1].className = "image active";
     currentIndex--;
+    changeNewDot(currentIndex);
 });
 
 //Fix bug
 dots.forEach((dot)=>{
     dot.addEventListener('click', () => {
         const index = Number(dot.className.slice(-1));
+        changePreviousDot(currentIndex);
         images[currentIndex].className = 'image';
         currentIndex = index;
         images[currentIndex].className = "image active";
+        changeNewDot(currentIndex);
     })
 })
 
